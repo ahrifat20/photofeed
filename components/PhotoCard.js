07b17aab03b1e26f2e0bc7e24a088b65865d8079
ahrifat20 <akborhossainrifat20@gmail.com>
2024-04-03 00:46:30 +0600

    card update

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function PhotoCard({ photo }) {
   return (
     <>
-      <Link href={`/photos/${photo.id}`} className="group">
+      <Link href={`api/photos/${photo.id}`} className="group">
         <Image src={photo.url} alt={photo.title} width={700} height={700} />
         <div className="title-container">
           <h4 className="title">{photo.title}</h4>
@@ -13,11 +13,3 @@ export default function PhotoCard({ photo }) {
     </>
   );
 }
-
-// export default function PhotoCard() {
-//   return (
-//     <>
-//       <h2>Welcome PhotoCard </h2>
-//     </>
-//   );
-// }
